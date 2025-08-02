ec11d636612df82d2a046ff6512d43ee90fd4c0b
webshum <webshumweb@gmail.com>
2025-08-02 12:40:17 +0300

    update

--- common/components/LanguageSelector.php
+++ common/components/LanguageSelector.php
@@ -9,20 +9,28 @@ class LanguageSelector implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        $session = Yii::$app->session;
-        if (!$session->isActive) {
-            $session->open();
+        // Не ініціалізуй сесію, якщо заголовки вже надіслані
+        if (!headers_sent()) {
+            $session = Yii::$app->session;
+
+            if (!$session->isActive) {
+                $session->open();
+            }
+
+            $availableLanguages = is_array(Yii::$app->params['lang']) ? array_keys(Yii::$app->params['lang']) : [];
+
+            $lang = Yii::$app->request->get('lang');
+
+            if ($lang && in_array($lang, $availableLanguages)) {
+                $session->set('language', $lang);
+            }
+
+            Yii::$app->language = $session->get('language', $_ENV['APP_LANG'] ?? 'en-US');
+        } else {
+            // fallback без сесії
+            Yii::$app->language = $_ENV['APP_LANG'] ?? 'en-US';
         }
-
-        $availableLanguages = is_array(Yii::$app->params['lang']) ? array_keys(Yii::$app->params['lang']) : [];
-
-        $lang = Yii::$app->request->get('lang');
-
-        if ($lang && in_array($lang, $availableLanguages)) {
-            $session->set('language', $lang);
-        }
-
-        Yii::$app->language = $session->get('language', $_ENV['APP_LANG'] ?? 'en-US');
     }
+
 }
 
